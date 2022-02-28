877db141b587f5847eb7e30b2e6aaa9ebec5c11a
MelissaCFox <melissa.caitlin.fox@gmail.com>
2022-02-28 11:33:50 -0600

    changed order_count filter to greater than instead of less than

--- db.sql
+++ db.sql
@@ -1,2 +1,4 @@
-delete from bangazon_api_paymenttype
-where id=9
+select * from bangazon_api_orderproduct op
+join bangazon_api_order  on bangazon_api_order.id = op.order_id
+where bangazon_api_order.completed_on != 'NULL'
+order by product_id
